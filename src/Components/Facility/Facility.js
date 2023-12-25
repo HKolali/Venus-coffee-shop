26114c263e5473e7b1d5b2fd65540ca0671d2ab4
Hossein Kolali <kolali.hossein@gmail.com>
2023-12-25 10:14:27 +0330

    add our facility section from coffee shop

--- src/Components/Facility/Facility.js
+++ src/Components/Facility/Facility.js
@@ -1,6 +1,14 @@
 import './Facility.css'
+import FacilityItem from './FacilityItem';
 
 function Facility(){
+    let facilities = [
+        {image: "./images/facility/facility-1.png", title: "Ready To Go Coffee", description:"Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Error, Eaque."},
+        {image: "./images/facility/facility-2.png", title: "Breakfast And Sweets", description:"Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Error, Eaque."},
+        {image: "./images/facility/facility-3.png", title: "Coffee Beans", description:"Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Error, Eaque."},
+        {image: "./images/facility/facility-4.png", title: "Varieties Of Coffees", description:"Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Error, Eaque."}
+    ];
+
     return(
         <div className="facility">
             <div className="facility__header">
@@ -8,7 +16,10 @@ function Facility(){
                 <h3 className="facility__header-title">Our Facility</h3>
             </div>
             <div className="facility__content">
-                
+                <FacilityItem {...facilities[0]}/>
+                <FacilityItem {...facilities[1]}/>
+                <FacilityItem {...facilities[2]}/>
+                <FacilityItem {...facilities[3]}/>
             </div>
         </div>
     );
--- src/Components/Facility/FacilityItem.js
+++ src/Components/Facility/FacilityItem.js
@@ -1,9 +1,11 @@
 import './FacilityItem.css'
 
-function FacilityItem(){
+function FacilityItem({image, title, description}){
     return(
         <div className="facility-item">
-            
+            <img src={image} alt="" className='facility-item-image'/>
+            <h3 className="facility-item-title">{title}</h3>
+            <p className="facility-item-description">{description}</p>
         </div>
     );
 }
